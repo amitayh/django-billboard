7a56c10187b20a66bdc5b6507284ec7d76179d19
Amitay Horwitz <amitayh@gmail.com>
2009-10-01 22:03:13 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,72 @@
+from django.db import models
+import mptt
+
+class Category(models.Model):
+    name            = models.CharField(max_length=255, unique=True)
+    description     = models.TextField(null=True, blank=True)
+    parent          = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    properties      = models.ManyToManyField('Property')
+    ads_count       = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['tree_id', 'lft']
+    
+    def __unicode__(self):
+        return u'%s' % self.name
+    
+    def get_properties(self):
+        pass
+
+mptt.register(Category)
+
+class Property(models.Model):
+
+    VALUE_TYPES = (
+        (0, 'Text'),
+        (1, 'Choice'),
+        (2, 'Tree'),
+    )
+    
+    name            = models.CharField(max_length=255, unique=True)
+    description     = models.TextField(null=True, blank=True)
+    type            = models.IntegerField(choices=VALUE_TYPES, default=0)
+    
+    def __unicode__(self):
+        return u'%s' % self.name
+
+class PropertyValue(models.Model):
+    value           = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+    
+    def __unicode__(self):
+        return u'%s' % self.value
+
+class PropertyValueText(PropertyValue):
+    pass
+
+class PropertyValueChoice(PropertyValue):
+    pass
+
+class PropertyValueTree(PropertyValue):
+    parent          = models.ForeignKey('self', null=True, blank=True, related_name='children')
+
+mptt.register(PropertyValueTree)
+
+class Ad(models.Model):
+    name            = models.CharField(max_length=255)
+    description     = models.TextField()
+    categories      = models.ManyToManyField(Category)
+    
+    def __unicode__(self):
+        return u'%s' % self.name
+    
+    def get_properties(self):
+        pass
+
+class AdPropertyValue(models.Model):
+    ad              = models.ForeignKey(Ad)
+    property        = models.ForeignKey(Property)
+    property_value  = models.IntegerField()
